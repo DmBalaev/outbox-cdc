238ef6f68c032540a20051bbc4e5aab44bc7fa42
Akira <@>
2025-01-11 16:47:01 +0300

    init project

---
+++ credit-service/src/test/java/dm/creditservice/CreditServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dm.creditservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
